16553c7748d9c70dabf4149c2bcc38396b416ffa
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-07-03 20:43:26 +0530

    callbacks

--- 6.http/exercise.js
+++ 6.http/exercise.js
@@ -1,6 +1,5 @@
 // const http = require('http');
 
-
 // const server = http.createServer((request, response) => {
 //   console.log(request.method, request.url, 'request received');
 //   const url = request.url;
@@ -19,7 +18,7 @@
 //     response.writeHead(404, { 'Content-Type': 'text/plain' });
 //     response.end('404 Not Found');
 //   }
-  
+
 // })
 
 // const port = 3000;
@@ -28,18 +27,18 @@
 //   console.log(`Server is now listening on port ${port}`);
 // })
 
-
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
   const url = request.url;
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello, World!');
-})
- 
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("helloworld \n");
+  response.write("hello world from node.js");
+  response.end();
+});
 
 const port = 3000;
 
 server.listen(port, () => {
   console.log(`Server is now listening on port ${port}`);
-})
+});
---
+++ callback/index.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+
+// const promise = new Promise((resolve, reject) => {
+//   fs.readFile('file.txt', 'utf8', (err, data) => {
+//     if (err) {
+//       reject(err);
+//     }
+//     resolve(data);
+//   });
+// });
+
+// promise.then((data) => {
+//   console.log(data);
+// }).catch((err) => {
+//   console.error('Error reading file:', err);
+// })
+
+
+// function person(name, callback) {
+//   console.log(`hello ${name}`)
+//   callback();
+// }
+
+
+// function address() {
+//   console.log('hello address')
+
+
+// }
+
+// person('John', address);
+
+
+
+// async function readFileAsync() {
+//   try {
+//     const data = await fs.promises.readFile('file.txt', 'utf8');
+//     console.log(data);
+//   } catch (err) {
+//     console.error('Error reading file:', err);
+//   }
+// }
+
+// readFileAsync();
+
+// async function readFile() {
+//   try {
+//     const data = await fs.promises.readFile('file.txt', 'utf8');
+//     console.log(data);
+//   } catch (error) {
+//     console.error('Error reading file:', error);
+//   }
+// }
+
+// readFile();
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`⏱️ Waited ${ms} milliseconds`);
+    }, ms);
+  });
+}
+
+async function doSteps() {
+  console.log("Step 1: Starting");
+
+  const result1 = await wait(1000); // wait 1 second
+  console.log("Step 2:", result1);
+
+  const result2 = await wait(2000); // wait 2 seconds
+  console.log("Step 3:", result2);
+
+  console.log("Step 4: All done 🎉");
+}
+
+doSteps();
