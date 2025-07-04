d70c30fba9f537a683852037905fc732e4109e10
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-07-04 23:31:46 +0530

    Update index.js

--- callback/index.js
+++ callback/index.js
@@ -54,24 +54,38 @@ const fs = require("fs");
 
 // readFile();
 
-function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`⏱️ Waited ${ms} milliseconds`);
-    }, ms);
-  });
+// function wait(ms) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`⏱️ Waited ${ms} milliseconds`);
+//     }, ms);
+//   });
+// }
+
+// async function doSteps() {
+//   console.log("Step 1: Starting");
+
+//   const result1 = await wait(1000); // wait 1 second
+//   console.log("Step 2:", result1);
+
+//   const result2 = await wait(2000); // wait 2 seconds
+//   console.log("Step 3:", result2);
+
+//   console.log("Step 4: All done 🎉");
+// }
+
+// doSteps();
+
+
+async function readFile() {
+  try {
+    const data = await fs.promises.readFile('file.txt', 'utf8');
+    console.log(data);
+    
+  } catch (error) {
+    console.error('Error reading file:', error);
+    
+  }
 }
 
-async function doSteps() {
-  console.log("Step 1: Starting");
-
-  const result1 = await wait(1000); // wait 1 second
-  console.log("Step 2:", result1);
-
-  const result2 = await wait(2000); // wait 2 seconds
-  console.log("Step 3:", result2);
-
-  console.log("Step 4: All done 🎉");
-}
-
-doSteps();
+readFile();
